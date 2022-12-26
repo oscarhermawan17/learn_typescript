b2834804a56616c434227e7d6a5fc3e56617438b
Oscar Hermawan <Oscar_H261@mitrais.com>
2022-12-26 21:04:46 +0800

    Section 06 - Array Types (done)

---
+++ Section_06_Array_Types/arraysExercise.js
@@ -0,0 +1,18 @@
+// **********************************************
+// ******************* PART 1 *******************
+// **********************************************
+// Create an empty array of numbers called "ages":
+var ages = [];
+// **********************************************
+// ******************* PART 2 *******************
+// **********************************************
+// Create an array variable called gameBoard that starts as an empty array.
+// It should be typed to hold a 2 dimensional array of strings
+var gameBoard = [];
+// **********************************************
+// ******************* PART 4 *******************
+// **********************************************
+// Write a function called getTotal that accepts an array of Product types
+// It should return the sum of all the products' prices
+var getTotal = function (products) { return products.reduce(function (total, product) { return product.price + total; }, 0); };
+console.log(getTotal([{ name: "coffee mug", price: 11.50 }, { name: "coffee mug", price: 11.50 }]));
